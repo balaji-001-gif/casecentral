615aa477ac5e2a6d731dcd1745d85d5c2be1e7d6
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-03-02 16:49:59 +0530

    Fix V15 'Use of sub-query or function is restricted' error on Nature of Case
    - case.js set_query was returning undefined when service field was empty
    - V15's stricter query validation rejects undefined return from set_query
    - Now always returns a valid {filters: {}} object

--- casecentral/case_central/doctype/case/case.js
+++ casecentral/case_central/doctype/case/case.js
@@ -2,54 +2,52 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Case', {
-	refresh: function(frm) {
+	refresh: function (frm) {
 		frm.set_query('nature_of_case', () => {
+			let filters = {};
 			if (frm.doc.service) {
-				return {
-					filters: {
-						'service': frm.doc.service
-					}
-				};
+				filters['service'] = frm.doc.service;
 			}
+			return { filters: filters };
 		});
 	},
-	after_save: function(frm) {
-		if(frm.doc.status == "Pending" && frm.doc.registration_number) {
+	after_save: function (frm) {
+		if (frm.doc.status == "Pending" && frm.doc.registration_number) {
 			frm.doc.status = "InProgress";
 		}
-		if(frm.doc.status=="Pending" || frm.doc.status=="InProgress") {
+		if (frm.doc.status == "Pending" || frm.doc.status == "InProgress") {
 			frm.doc.documents_handover = 0;
 		}
 	},
-	petitioner: function(frm){
+	petitioner: function (frm) {
 		if (frm.doc.petitioner && frm.doc.respondent) {
 			frm.set_value("case_title", frm.doc.petitioner + " V/s " + frm.doc.respondent);
 		}
 	},
-	respondent: function(frm){
+	respondent: function (frm) {
 		if (frm.doc.petitioner && frm.doc.respondent) {
 			frm.set_value("case_title", frm.doc.petitioner + " V/s " + frm.doc.respondent);
 		}
 	},
-	ecase_type: function(frm){
+	ecase_type: function (frm) {
 		var ct_str = frm.doc.ecase_type.split("-")[0].trim().replace(/\./g, "");
 		frm.set_value("case_type_abbr", ct_str);
 		set_registration_number(frm);
 	},
-	case_no: function(frm){
+	case_no: function (frm) {
 		set_registration_number(frm);
 	},
-	case_year: function(frm){
+	case_year: function (frm) {
 		set_registration_number(frm);
 	}
 });
 
 frappe.ui.form.on('Case History', {
-	case_history_add: function(frm, cdt, cdn) {
+	case_history_add: function (frm, cdt, cdn) {
 		var rows = Object.entries(locals[cdt]);
 		var current_row = locals[cdt][cdn];
 		// The index in locals starts from 1 whereas in rows it starts from 0 as such 2 is subtracted
-    	var previous_row = locals[cdt][rows[current_row.idx - 2][0]];
+		var previous_row = locals[cdt][rows[current_row.idx - 2][0]];
 		if (previous_row) {
 			current_row.judge = previous_row.judge;
 			current_row.business_on_date = previous_row.hearing_date;
@@ -59,7 +57,7 @@ frappe.ui.form.on('Case History', {
 	}
 });
 
-var set_registration_number = function(frm) {
+var set_registration_number = function (frm) {
 	if (frm.doc.case_type_abbr && frm.doc.case_no && frm.doc.case_year) {
 		frm.set_value("registration_number", frm.doc.case_type_abbr + "_" + frm.doc.case_no + "_" + frm.doc.case_year);
 	} else {
